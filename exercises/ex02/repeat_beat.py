5971ad14499701e20d5d5877231b2692c313356e
Carrie Shive <csshive@ad.unc.edu>
2021-09-08 20:44:40 -0400

    Progress on Exercise 1

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -4,11 +4,11 @@ __author__ = "730330844"
 
 counter: int = 0
 user_string_beat: str = input("What beat do you want to repeat? ")
-user_string_number: str = int(input("How many times do you want to repeat it? "))
+user_string_number: str = input("How many times do you want to repeat it? ")
 
-while counter < user_string_number:
-    print(user_string_beat + "" * user_string_number)
+while counter < int(user_string_number):
+    print(user_string_beat + "" * int(user_string_number))
     counter = counter + 1
 
-if user_string_number <= 0: 
+if int(user_string_number) <= 0: 
     print("No beat...") 
