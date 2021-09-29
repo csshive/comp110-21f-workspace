4f24af932bdedcdac07155a19a85a40507951259
Carrie Shive <csshive@ad.unc.edu>
2021-09-28 23:51:43 -0400

    progress

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,27 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730330844"
 
 
-# TODO: Implement your functions here.
+def all(num: int, list_of_ints: list[int]) -> bool: 
+    """Return True iff neddle is found in the list of ints. """
+    i: int = 0 
+    while i < len(list_of_ints): 
+        item: int = list_of_ints[i]
+        if item == num:
+            return True
+        i += 1
+    return False
+
+
+def is_equal(list_one: list[int], list_two: list[int]) -> bool:
+    i: int = 0
+    if len(list_one) != len(list_two):
+        return False  
+    while i < len(list_one):
+        item: int = list_one[i]
+        item_two: int = list_two[i]
+        if item == item_two 
+            i += 1 
+            
+             
