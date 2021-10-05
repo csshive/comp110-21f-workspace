99f4e9c4fea5f067420f6cf928690104c3d260ae
Carrie Shive <csshive@ad.unc.edu>
2021-10-05 19:14:53 -0400

    progreess

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,33 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730330844"
 
-# Define your functions below
+
+def only_evens(list_of_int: list[int]) -> list[int]: 
+    """Return a list containing only the elements of the input list that were even."""
+    evens = []
+    for number in list_of_int:
+        if number % 2 == 0:
+            evens.append(number)
+    return evens
+
+
+def sub(a_list: list[int], start_index: int, end_index: int) -> list[int]:
+    """Generates a List which is a subset of the given list, between the specified start index and the end index."""
+    new_list: list[int] = []
+    if len(a_list) == 0:
+        return new_list
+    if start_index > len(a_list):
+        return new_list
+    if end_index <= 0: 
+        return new_list
+    else:
+        new_list = a_list[start_index:end_index]
+        return new_list
+
+
+def concat(list_one_of_int: list[int], list_two_of_int: list[int]) -> list[int]:
+    """Generate a new List which contains all of the elements of the first list followed by all of the elements of the second list.""" 
+    for i in list_two_of_int:
+        list_one_of_int.append(i)
+    return list_one_of_int
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -3,4 +3,77 @@
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730330844"
+
+from exercises.ex05.utils import only_evens
+from exercises.ex05.utils import sub
+from exercises.ex05.utils import concat
+
+
+def test_only_evens_empty() -> None:
+    """Testing when function receives a list with no even numbers it returns an empty list."""
+    xs: list[int] = ([1, 5, 3])
+    assert only_evens(xs) == []
+
+
+def test_only_evens_single_item() -> None: 
+    """When list is empty function returns an empty list."""
+    xs: list[int] = ([])
+    assert only_evens(xs) == []
+
+
+def test_only_evens_items() -> None:
+    """When function has multiple even items it returns all items.""" 
+    xs: list[int] = [4, 4, 4]
+    assert only_evens(xs) == [4, 4, 4]
+
+
+def test_only_evens_items_again() -> None:
+    """When one even number is found function returns list with item."""
+    xs: list[int] = [1, 2, 3]
+    assert only_evens(xs) == [2]
+
+
+def test_sub_empty() -> None:
+    """When list is empty function returns an empty list."""
+    a_list: list[int] = ([])
+    start_index: int = 1
+    end_index: int = 3
+    assert sub(a_list, start_index, end_index) == []
+
+
+def test_sub_items() -> None:
+    """If start is greater than the length of the list returns empty list."""
+    a_list: list[int] = ([1, 2, 3, 4])
+    start_index: int = 5
+    end_index: int = 3
+    assert sub(a_list, start_index, end_index) == []
+
+
+def test_sub_again() -> None:
+    """When one even number is found function returns list with item."""
+    a_list: list[int] = ([10, 20, 30, 40])
+    start_index: int = 1
+    end_index: int = 3
+    assert sub(a_list, start_index, end_index) == [20, 30]
+
+
+def test_concat_empty() -> None:
+    """When list is empty function returns an empty list."""
+    list_one_of_int: list[int] = ([])
+    list_two_of_int: list[int] = ([])
+    assert concat(list_one_of_int, list_two_of_int) == []
+
+
+def test_concat_items() -> None:
+    """Two list of same integer."""
+    list_one_of_int: list[int] = ([2, 2, 2])
+    list_two_of_int: list[int] = ([2, 2, 2])
+    assert concat(list_one_of_int, list_two_of_int) == [2, 2, 2, 2, 2, 2]
+
+
+def test_concat_again() -> None:
+    """One multiple list one empty list."""
+    list_one_of_int: list[int] = ([2, 3, 4, 7])
+    list_two_of_int: list[int] = ([])
+    assert concat(list_one_of_int, list_two_of_int) == [2, 3, 4, 7]
