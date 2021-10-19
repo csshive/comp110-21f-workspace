fb380f5d0c6aa321a04d00535e395fac89b3b9e1
Carrie Shive <csshive@ad.unc.edu>
2021-10-19 16:00:23 -0400

    progess

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,32 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730330844"
 
-# Define your functions below
+
+def invert(list_of_str: dict[str, str]) -> dict[str, str]: 
+    """Program to invert a list."""
+    reversed_dictionary = {value: key for (key, value) in list_of_str.items()}
+    return(reversed_dictionary)
+
+
+def favorite_color(names_colors: dict[str, str]) -> str:
+    """Program to identify the favorite color."""
+    track = {}
+    for key, value in names_colors.items():
+        if value not in track:
+            track[value] = 0
+    else: 
+        for key, value in names_colors.items():
+            track[value] += 1
+    return(max(track, key=track.get))
+
+
+def count(string_list: list[str]) -> dict[str, int]:
+    """Program to count frequency."""
+    freq = {}
+    for item in string_list:
+        if (item in freq):
+            freq[item] += 1
+        else:
+            freq[item] = 1
+    return(freq)
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,55 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+from exercises.ex06.dictionaries import invert, favorite_color, count
+
+__author__ = "730330844"
+
+
+"""Test for the sum function."""
+
+
+def test_invert_first() -> None: 
+    xs: dict[str, str] = {'a': 'z', 'b': 'y', 'c': 'x'}
+    assert invert(xs) == {'z': 'a', 'y': 'b', 'x': 'c'}
+
+
+def test_invert_two() -> None: 
+    xs: dict[str, str] = {'apple': 'cat'}
+    assert invert(xs) == {'cat': 'apple'}
+
+
+def test_invert_three() -> None: 
+    xs: dict[str, str] = {'kris': 'jordan', 'michael': 'bill'}
+    assert invert(xs) == {'jordan': 'kris', 'bill': 'michael'}
+
+
+def test_favorite_color_first() -> None: 
+    xs: dict[str, str] = {"Marc": "yellow", "Ezri": "blue", "NCSU": "blue"}
+    assert favorite_color(xs) == str("blue")
+
+
+def test_favorite_color_two() -> None: 
+    xs: dict[str, str] = {"Marc": "green", "Ezri": "green", "NCSU": "green"}
+    assert favorite_color(xs) == str("green")
+
+
+def test_favorite_color_three() -> None: 
+    xs: dict[str, str] = {'Mat': 'yellow'}
+    assert favorite_color(xs) == str("yellow")
+
+
+def test_count_first() -> None: 
+    xs: list[str] = ["a", "a", "a", "b", "b", "b", "c", "c", "c"]
+    assert count(xs) == {"a": 3, "b": 3, "c": 3}
+
+
+def test_count_two() -> None: 
+    xs: list[str] = ["a", "a", "b", "b", "b", "c"]
+    assert count(xs) == {"a": 2, "b": 3, "c": 1}
+
+
+def test_count_three() -> None: 
+    xs: list[str] = ["a", "a", "a", "b", "c", "c", "c"]
+    assert count(xs) == {"a": 3, "b": 1, "c": 3}
